438974d05cfb2eb537d9577dcb9de5dec0e2fa14
Alexey Hohlov <root@amper.me>
2017-09-21 23:16:20 +0300

    MTC-KEYS: список разрешенных кейкодов

--- mtc-keys.c
+++ mtc-keys.c
@@ -1,4 +1,36 @@
+#include <linux/input.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/timer.h>
+#include <linux/workqueue.h>
 
+static int mtc_keycodes[] = {
+    KEY_NUMERIC_0,
+    KEY_BACK,
+    KEY_NUMERIC_2,
+    KEY_NUMERIC_3,
+    KEY_NUMERIC_4,
+    KEY_NUMERIC_5,
+    KEY_NUMERIC_6,
+    KEY_NUMERIC_7,
+    KEY_NUMERIC_8,
+    KEY_NUMERIC_9,
+    KEY_NUMERIC_STAR,
+    KEY_NUMERIC_POUND,
+    0x20E,
+    0x20F,
+    KEY_CAMERA_FOCUS,
+    KEY_TOUCHPAD_ON,
+    KEY_HOME,
+    KEY_MENU,
+    KEY_VOLUMEDOWN,
+    KEY_VOLUMEUP,
+    KEY_TOUCHPAD_OFF,
+    KEY_CAMERA_ZOOMIN,
+    KEY_CAMERA_ZOOMOUT,
+    KEY_CAMERA_UP,
+    KEY_CAMERA_DOWN,
+};
 
 static int
 keys_suspend(struct device *dev)
