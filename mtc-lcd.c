8d24557271c71ddcd0f61d30f7dad869a4c75d0e
Alexey Hohlov <root@amper.me>
2017-12-26 21:02:09 +0300

    MTC-LCD: adding new functions

--- mtc-lcd.c
+++ mtc-lcd.c
@@ -1,15 +1,79 @@
+#include <linux/module.h>
+#include <linux/time.h>
+#include <linux/device.h>
 
+#include "mtc_shared.h"
 
 static int
-lcd_suspend(struct device *dev)
+lcd_suspend(struct device * dev)
 {
+	(void)dev;
+
 	return 0;
 }
 
 static int
-lcd_resume(struct device *dev)
+lcd_resume(struct device * dev)
 {
+	(void)dev;
+
 	return 0;
+}
+
+/* decompiled */
+static void
+lcd_delay()
+{
+	long usec; // r4@1
+	long check; // r3@2
+	struct timeval tv;
+
+	do_gettimeofday(&tv);
+	usec = tv.tv_usec;
+	do
+	{
+		do_gettimeofday(&tv);
+		check = tv.tv_usec - usec;
+		if ( usec > tv.tv_usec )
+		{
+			check = tv.tv_usec + 1000000 - usec;
+		}
+	}
+	while ( check <= 5 );
+}
+
+/* decompiled */
+static void
+lcd_send_cmd(int16_t cmd)
+{
+	int16_t bit_pos;
+
+	bit_pos = (cmd & 0x1FF) | 0x800;
+	gpio_direction_output(gpio_LCD_CS, 0);
+	lcd_delay();
+
+	for (unsigned int bits = 12; bits > 0; bits--)
+	{
+		int bit = 1;
+
+		gpio_direction_output(gpio_LCD_CLK, 0);
+
+		if ( !(bit_pos & 0x800) )
+		{
+			bit = 0;
+		}
+
+		gpio_direction_output(gpio_LCD_DAT, bit);
+
+		lcd_delay();
+		bit_pos *= 2;
+		gpio_direction_output(gpio_LCD_CLK, 1);
+		lcd_delay();
+	}
+
+	gpio_direction_output(gpio_LCD_CS, 1);
+	gpio_direction_output(gpio_LCD_DAT, 1);
+	lcd_delay();
 }
 
 static int
